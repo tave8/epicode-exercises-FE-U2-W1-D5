f92024aa7061e33ebfa706103236cd3e2b0193d9
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-01-10 01:11:33 +0100

    on mobile, hero is good

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,31 @@
 // when scroll reaches end of hero minus x, toggle the navbar animation
 const main = () => {
-    addEventNavbarScroll()
-}
+  addEventNavbarScroll();
+};
 
-window.addEventListener("load", main)
-
+window.addEventListener("load", main);
 
 const addEventNavbarScroll = () => {
-    
-    // get the navbar
-    const navbar = document.querySelector("header > .navbar")
-    // get the hero
-    const hero = document.querySelector("header > .hero")
-    // get the hero y coordinate of bottom border
+  // get the navbar
+  const navbar = document.querySelector("header > .navbar");
+  // get the hero
+  const hero = document.querySelector("header > .hero");
+  // get the hero y coordinate of bottom border
 
-    // if there's a scroll, and the y coordinate of the hero's
-    // bottom border is less than the actual position of where the 
-    // viewport is at right now, activate the animation
+  // if there's a scroll, and the y coordinate of the hero's
+  // bottom border is less than the actual position of where the
+  // viewport is at right now, activate the animation
 
-    window.addEventListener("scroll", (event) => {
-        // check if y scroll is > or < than hero - x 
-        const heroCoordinates = hero.getBoundingClientRect()
-        console.log(heroCoordinates.bottom)
-        if (heroCoordinates.bottom < 150) {
-            // console.log("activate animation")
-            navbar.classList.add("navbar-animate")
-        }
-        else {
-            // navbar.classList.remove("navbar-animate")
-
-            // console.log("deactivate animation")
-        }
-    })
-}
+  window.addEventListener("scroll", (event) => {
+    // check if y scroll is > or < than hero - x
+    const heroCoordinates = hero.getBoundingClientRect();
+    console.log(heroCoordinates.bottom);
+    if (heroCoordinates.bottom < 150) {
+      // console.log("activate animation")
+      // navbar.classList.add("navbar-animate")
+    } else {
+      // navbar.classList.remove("navbar-animate")
+      // console.log("deactivate animation")
+    }
+  });
+};
